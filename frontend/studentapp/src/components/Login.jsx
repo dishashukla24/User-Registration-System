1a24509bebeb67ad45e9cd456409e20404ac0377
Disha <disha@Dishas-MacBook-Air.local>
2025-04-23 10:00:09 +0530

    user_registration_system

---
+++ frontend/studentapp/src/components/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>Login1
+        <label>: <input type="text" name="username" /></label>
+        <label >Password: <input type="text" /></label>
+    </div>
+  )
+}
+
+export default Login
