8628c1b495cbee86d4dd4561f9544ad0fd7c50f8
Disha <disha@Dishas-MacBook-Air.local>
2025-04-28 09:59:11 +0530

    atlas

--- backend/database.js
+++ backend/database.js
@@ -1,6 +1,6 @@
 const mongoose =require('mongoose');
 async function dbConn(){
-    const conn=await mongoose.connect('mongodb://localhost:27017/studentapp');
+    const conn=await mongoose.connect('mongodb+srv://disha:root@cluster0.ljeukk7.mongodb.net/abes-crud?retryWrites=true&w=majority&appName=Cluster0');
     if(conn){
         console.log('database connect successfully')
 
